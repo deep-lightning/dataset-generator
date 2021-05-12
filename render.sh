0f3440b1238782b272d365abbf6db214aad3bca5
Yawee <julio@tryolabs.com>
2021-05-12 00:58:42 -0300

    Changed script to hard shadows

--- render.sh
+++ render.sh
@@ -1,8 +1,12 @@
 # $1 mode
 # $2 path
+function filter {
+    pfilt -m .25 -x /2 -y /2 $2/$1.unf > $2/$1.hdr
+}
+
 function render {
     rpict @config/$1 -z $2/$1.zbf $2/scene.oct > $2/$1.unf
-    pfilt -m .25 -x /8 -y /8 $2/$1.unf > $2/$1.hdr
+    filter $1 $2
     convert $2/$1.hdr $3/$1.png
 }
 
@@ -20,7 +24,7 @@ function loop {
     
     echo "Generating diffuse map"
     rpict @config/diffuse -z $meta/diffuse.zbf $meta/scene_wo_light.oct > $meta/diffuse.unf
-    pfilt -m .25 -x /8 -y /8 $meta/diffuse.unf > $meta/diffuse.hdr
+    filter "diffuse" $meta
     convert $meta/diffuse.hdr $path/diffuse.png
 
     echo "Generating local illumination"
@@ -31,16 +35,19 @@ function loop {
 
     echo "Generating depth buffer"
     pvalue -h `getinfo -d < $meta/global.unf` -r -b -df $meta/global.zbf | falsecolor -lw 0 -m 1 -s 10 -l Meters -r v -g v -b v > $meta/z.unf
-    pfilt -m .25 -x /8 -y /8 $meta/z.unf > $meta/z.hdr
+    filter "z" $meta
     Ra_bmp $meta/z.hdr > $meta/z.bmp
     convert $meta/z.bmp $path/z.png
 
     echo "Generating normal buffer"
     vwrays -ff $meta/global.unf | rtrace -w -ffa -on $meta/scene.oct > $meta/normal.pts
     getinfo -c rcalc -oa -e '$1=($1+1)/2;$2=($2+1)/2;$3=($3+1)/2' < $meta/normal.pts | pvalue -r -da `getinfo -d < $meta/global.unf` > $meta/normal.unf
-    pfilt -m .25 -x /8 -y /8 $meta/normal.unf > $meta/normal.hdr
+    filter "normal" $meta
     Ra_bmp $meta/normal.hdr > $meta/normal.bmp
     convert $meta/normal.bmp $path/normal.png
+
+    # cleanup
+    rm -r $meta
 }
 
 # $1 input folder
