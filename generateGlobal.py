f8a26bdd9bed96060e5dcc63c05abd2c3b8cc14e
Yawee <julio@tryolabs.com>
2021-08-29 19:48:52 -0300

    Fix indirect scripts

--- generateGlobal.py
+++ generateGlobal.py
@@ -1,17 +1,20 @@
 import cv2
 import os
 
-for dataset in os.listdir('dataset'):
-  if not dataset.startswith('.'):
-    datasetPath = os.path.join(dir, dataset)
-    for subdir in os.listdir(datasetPath):
-      subpath = os.path.join(datasetPath, subdir)
-      globalPath = os.path.join(subpath, 'global.png')
-      localPath = os.path.join(subpath, 'local.png')
-      indirectPath = os.path.join(subpath, 'indirect.png')
+DATASET_FOLDER = "diffuse"
 
-      indirectImg = cv2.imread(indirectPath, 0)
-      localImg = cv2.imread(localPath, 0)
+for dataset in os.listdir(DATASET_FOLDER):
+    if not dataset.startswith("."):
+        datasetPath = os.path.join(DATASET_FOLDER, dataset)
+        for subdir in os.listdir(datasetPath):
+            subpath = os.path.join(datasetPath, subdir)
+            globalPath = os.path.join(subpath, "globalCheck.png")
+            localPath = os.path.join(subpath, "local.png")
+            indirectPath = os.path.join(subpath, "indirect.png")
 
-      globalImg = cv2.add(indirectImg, localImg)
-      cv2.imwrite(globalPath, globalImg)
+            indirectImg = cv2.imread(indirectPath)
+            localImg = cv2.imread(localPath)
+
+            globalImg = cv2.add(indirectImg, localImg)
+
+            cv2.imwrite(globalPath, globalImg)
--- generateIndirect.py
+++ generateIndirect.py
@@ -1,17 +1,20 @@
 import cv2
 import os
 
-for dataset in os.listdir('dataset'):
-  if not dataset.startswith('.'):
-    datasetPath = os.path.join('dataset', dataset)
-    for subdir in os.listdir(datasetPath):
-      subpath = os.path.join(datasetPath, subdir)
-      globalPath = os.path.join(subpath, 'global.png')
-      localPath = os.path.join(subpath, 'local.png')
-      indirectPath = os.path.join(subpath, 'indirect.png')
+DATASET_FOLDER = "diffuse"
 
-      globalImg = cv2.imread(globalPath, 0)
-      localImg = cv2.imread(localPath, 0)
+for dataset in os.listdir(DATASET_FOLDER):
+    if not dataset.startswith("."):
+        datasetPath = os.path.join(DATASET_FOLDER, dataset)
+        for subdir in os.listdir(datasetPath):
+            subpath = os.path.join(datasetPath, subdir)
+            globalPath = os.path.join(subpath, "global.png")
+            localPath = os.path.join(subpath, "local.png")
+            indirectPath = os.path.join(subpath, "indirect.png")
 
-      indirect = cv2.absdiff(globalImg, localImg)
-      cv2.imwrite(indirectPath, indirect)
+            globalImg = cv2.imread(globalPath)
+            localImg = cv2.imread(localPath)
+
+            indirect = cv2.subtract(globalImg, localImg)
+
+            cv2.imwrite(indirectPath, indirect)
