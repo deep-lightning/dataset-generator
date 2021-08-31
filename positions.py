7a46766f7b533b8603b7d96dbe058fe0da4028fb
Yawee <julio@tryolabs.com>
2021-08-31 01:07:36 -0300

    Remove y rotation to avoid dragon near the ceiling

--- positions.py
+++ positions.py
@@ -12,12 +12,15 @@ for x in range(amount):
     tx = random.uniform(-0.5, 0.5)
     ty = random.uniform(-0.5, 0.5)
 
-    rx = random.choice(range(0,360,1))
-    ry = random.choice(range(0,360,1))
-    rz = random.choice(range(0,360,1))
+    rx = random.choice(range(0, 360, 1))
+    # ry = random.choice(range(0, 360, 1))
+    rz = random.choice(range(0, 360, 1))
 
-    subprocess.run(f"xform -t {tx} {ty} 0 -rx {rx} -ry {ry} -rz {rz} ./base_input/{model}.rad > ./input/{model}_{x}.rad", shell=True)
-    values.add((tx,ty,rx,ry,rz))
+    subprocess.run(
+        f"xform -t {tx} 0 {ty} -rx {rx} -ry 0 -rz {rz} ./base_input/{model}.rad > ./input/{model}_{x}.rad",
+        shell=True,
+    )
+    values.add((tx, ty, rx, rz))
 
 if len(values) == amount:
     print("All values different")
