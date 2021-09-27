c0625a4e1607065c96aca21a588edccf3c632794
Yawee <julio@tryolabs.com>
2021-09-26 22:08:53 -0300

    Added camera and light variations

--- positions.py
+++ positions.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import random
 import subprocess
@@ -22,9 +23,35 @@ for x in range(amount):
     )
     values.add((tx, ty, rx, rz))
 
+    # add configs for camera
+    radius = 3.41421356
+    rot_hor = random.choice(range(225, 315, 1))
+
+    pos_x = math.cos(math.radians(rot_hor)) * radius
+    pos_y = math.sin(math.radians(rot_hor)) * radius
+    pos_z = 0
+
+    norm_dir = math.sqrt((pos_x ** 2 + pos_y ** 2 + pos_z ** 2))
+    dir_x = -pos_x / norm_dir
+    dir_y = -pos_y / norm_dir
+    dir_z = -pos_z / norm_dir
+
+    up_x = 0
+    up_y = 0
+    up_z = 1
+
+    with open(f"./per_config/{x}", "w") as f:
+        f.writelines(
+            [
+                f"-vp {pos_x} {pos_y} {pos_z}\n",
+                f"-vd {dir_x} {dir_y} {dir_z}\n",
+                f"-vu {up_x} {up_y} {up_z}\n",
+            ]
+        )
+
 if len(values) == amount:
     print("All values different")
 else:
     print("At least a value is repeated")
 
-print(values, len(values), amount, len(values) == amount)
+# print(values, len(values), amount, len(values) == amount)
--- render.sh
+++ render.sh
@@ -16,34 +16,45 @@ function render {
       scene=$meta/scene.oct
     fi
 
-    rpict ${use_gpm} ${use_cpm} ${use_vpm} @config/$1 -z $meta/$1.zbf $scene > $meta/$1.unf
+    if [ $2 == "no" ]; then
+      rpict ${use_gpm} ${use_cpm} ${use_vpm} @config/$1 -z $meta/$1.zbf $scene > $meta/$1.unf
+    else
+      rpict ${use_gpm} ${use_cpm} ${use_vpm} @config/$1 @per_config/$file_num -z $meta/$1.zbf $scene > $meta/$1.unf
+    fi
     filter $1
 }
 
 # $1 file
 # $2 output directory
-# $3 if needs caustic photon map
-# $4 if needs volume photon map
+# $3 light position
+# $4 multiple cameras
+# $5 if needs caustic photon map
+# $6 if needs volume photon map
 function loop {
     filename=${1##*/}       # remove until /
     name=${filename%.rad}   # remove trailing .rad
-    path="$2/$name"
+    
+    lights=($3)
+    file_num=${name//[!0-9]/}
+    chosen=${lights[$((file_num % ${#lights[@]}))]}
+    
+    path="$2/${name}_$chosen"
     mkdir -p $path
 
-    meta="meta/$name"
+    meta="meta/${name}_$chosen"
     mkdir -p $meta
 
-    oconv materials.rad cornell_light.rad raw_cornell.rad $1 > $meta/scene.oct
+    oconv materials.rad lights/cornell_light_$chosen.rad raw_cornell.rad $1 > $meta/scene.oct
     oconv materials.rad raw_cornell.rad $1 > $meta/scene_wo_light.oct
 
     make_gpm="-apg $meta/global_photon_map.gpm 5k"
     use_gpm="-ap $meta/global_photon_map.gpm 50"
-    if [ "$3" = "cpm" ]; then
+    if [ "$5" = "cpm" ]; then
       echo "Using caustic photon map"
       make_cpm="-apc $meta/caustic_photon_map.vpm 5k"
       use_cpm="-ap $meta/caustic_photon_map.vpm 50"
     fi
-    if [ "$4" = "vpm" ]; then
+    if [ "$6" = "vpm" ]; then
       echo "Using volume photon map"
       make_vpm="-apv $meta/volume_photon_map.vpm 5k -ma 1 1 1 -mg 1"
       use_vpm="-ap $meta/volume_photon_map.vpm 50"
@@ -53,13 +64,13 @@ function loop {
     mkpmap ${make_gpm} ${make_cpm} ${make_vpm} -t 60 $meta/scene.oct
 
     echo "Generating diffuse map"
-    render "diffuse"
+    render "diffuse" $4
 
     echo "Generating local illumination"
-    render "local"
+    render "local" $4
 
     echo "Generating global illumination"
-    render "global"
+    render "global" $4
 
     echo "Generating depth buffer"
     pvalue -h `getinfo -d < $meta/global.unf` -r -b -df $meta/global.zbf | falsecolor -lw 0 -m 1 -s 10 -l Meters -r v -g v -b v > $meta/z.unf
@@ -80,6 +91,7 @@ function loop {
 
 # $1 input folder
 # $2 output folder
+# $3 light directions ex: up down left right back
 n=0
 N=15
 for file in $1/*.rad
@@ -88,8 +100,7 @@ do
     wait
     n=0
   fi
-    echo $file
-    loop $file $2 $3 $4 &
-    n=$(( n + 1 ))
+  loop $file $2 "${3:-up}" ${4:-no} $5 $6 &
+  n=$(( n + 1 ))
 done
 wait
