4015e0bab98eb5e80264b63c8e48c6dd48b20445
Julio Morero <julio@tryolabs.com>
2021-11-23 01:07:34 -0300

    Add time measurements and improve render script

--- render_with_times.sh
+++ render_with_times.sh
@@ -30,7 +30,17 @@ function render {
 # $4 multiple cameras
 # $5 if needs caustic photon map
 # $6 if needs volume photon map
+
+total_pre=0
+total_diffuse=0
+total_local=0
+total_global=0
+total_normal=0
+total_depth=0
+total_indirect=0
 function loop {
+    ts=$(date +%s%N)
+
     filename=${1##*/}       # remove until /
     name=${filename%.rad}   # remove trailing .rad
     
@@ -62,28 +72,42 @@ function loop {
 
     echo "Generating photon maps"
     mkpmap ${make_gpm} ${make_cpm} ${make_vpm} -t 60 $meta/scene.oct
+    
+    total_pre=$((total_pre + ($(date +%s%N) - $ts)/1000000))
 
+    ts=$(date +%s%N)
     echo "Generating diffuse map"
     render "diffuse" $4
+    total_diffuse=$((total_diffuse + ($(date +%s%N) - $ts)/1000000))
 
+    ts=$(date +%s%N)
     echo "Generating local illumination"
     render "local" $4
+    total_local=$((total_local + ($(date +%s%N) - $ts)/1000000))
 
+    ts=$(date +%s%N)
     echo "Generating global illumination"
     render "global" $4
+    total_global=$((total_global + ($(date +%s%N) - $ts)/1000000))
 
+    ts=$(date +%s%N)
     echo "Generating depth buffer"
-    pvalue -h `getinfo -d < $meta/global.unf` -r -b -df $meta/global.zbf | falsecolor -lw 0 -m 1 -s 10 -l Meters -r v -g v -b v > $meta/z.unf
+    pvalue -h `getinfo -d < $meta/global.unf` -r -b -df $meta/local.zbf | falsecolor -lw 0 -m 1 -s 10 -l Meters -r v -g v -b v > $meta/z.unf
     filter "z"
+    total_depth=$((total_depth + ($(date +%s%N) - $ts)/1000000))
 
+    ts=$(date +%s%N)
     echo "Generating normal buffer"
     vwrays -ff $meta/global.unf | rtrace -w -fff -on $meta/scene.oct > $meta/normal.pts
     getinfo -c rcalc -if3 -of -e '$1=($1+1)/2;$2=($2+1)/2;$3=($3+1)/2' < $meta/normal.pts | pvalue -r -df `getinfo -d < $meta/global.unf` > $meta/normal.unf
     filter "normal"
+    total_normal=$((total_normal + ($(date +%s%N) - $ts)/1000000))
 
+    ts=$(date +%s%N)
     echo "Generating indirect buffer"
     pcomb $meta/global.unf -s -1 $meta/local.unf > $meta/indirect.unf
     filter "indirect"
+    total_indirect=$((total_indirect + ($(date +%s%N) - $ts)/1000000))
 
     # cleanup
     rm -r $meta
@@ -93,14 +117,15 @@ function loop {
 # $2 output folder
 # $3 light directions ex: up down left right back
 n=0
-N=15
+N=1
 for file in $1/*.rad
 do
-  if [ "$n" -eq "$N" ]; then
-    wait
-    n=0
-  fi
-  loop $file $2 "${3:-up}" ${4:-no} $5 $6 &
+  # if [ "$n" -eq "$N" ]; then
+  #   wait
+  #   n=0
+  # fi
+  loop $file $2 "${3:-up}" ${4:-no} $5 $6
   n=$(( n + 1 ))
 done
+echo $total_pre $total_diffuse $total_local $total_global $total_normal $total_depth $total_indirect
 wait
